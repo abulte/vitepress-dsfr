431084303b143364d4526f00fa5c640570a40933
Alexandre Bulté <alexandre@bulte.net>
2021-11-10 17:34:25 +0100

    custom layout

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,6 @@
+<template>
+  <DsfrHeader />
+  <Content />
+  <DsfrFooter />
+  <Debug />
+</template>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,13 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
 import VueDsfr from '@laruiss/vue-dsfr'
+import Layout from './Layout.vue'
 
 import '@laruiss/vue-dsfr/dist/vue-dsfr.css'
 
 export default {
   ...DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
       app.use(VueDsfr)
   }
